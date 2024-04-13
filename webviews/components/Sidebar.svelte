c52e57d6914e0f22a7cf81d0147c8b9359e4688e
dhuriamey02 <dhuriamey02@gmail.com>
2024-04-13 19:37:58 +0530

    List in Sidebar with completed status toggle

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,7 +1,32 @@
-<div>Hello form Sidebar</div>
+<script lang="ts">
+  let todos: Array<{ text: string; completed: boolean }> = [];
+  let text = "";
+</script>
+
+<div>VS TODO list</div>
+<form
+  on:submit|preventDefault={() => {
+    todos = [{ text, completed: false }, ...todos];
+    text = "";
+  }}
+>
+  <input bind:value={text} />
+</form>
+<ul>
+  {#each todos as todo (todo.text)}
+    <li
+      class:complete={todo.completed}
+      on:click={() => {
+        todo.completed = !todo.completed;
+      }}
+    >
+      {todo.text}
+    </li>
+  {/each}
+</ul>
 
 <style>
-  div {
-    color: blue;
+  .complete {
+    text-decoration: line-through;
   }
 </style>
